40c0dda41c0bbed9e41d41d076e6c6d9778d7288
Christian Hoffmann <chris.hoffmann84@gmail.com>
2024-12-06 10:14:41 +0100

    🚀

---
+++ src/PotaShell.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { AppShell, Burger, Group, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { HamInput } from "./components/HamInput";
+
+function PotaShell() {
+    const [opened, { toggle }] = useDisclosure();
+
+    const [call, setCall] = useState("DN9CVR");
+    const [lat, setLat] = useState(50.819649);
+    const [long, setLong] = useState(6.97578);
+
+    const findParks = async () => {
+        const resp = await fetch(
+            "https://api.pota.app/park/grids/" +
+                (lat - 1) +
+                "/" +
+                (long + 1) +
+                "/" +
+                (lat + 1) +
+                "/" +
+                (long + 1) +
+                "/0"
+        );
+        const json = await resp.json();
+        console.log(json.features.length, json);
+    };
+
+    return (
+        <AppShell
+            header={{ height: 48 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+            }}
+            padding="md">
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                    <Title order={2}>next POTA</Title>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">
+                <HamInput
+                    call={call}
+                    setCall={setCall}
+                    lat={lat}
+                    setLat={setLat}
+                    long={long}
+                    setLong={setLong}
+                    onFindParksClicked={findParks}
+                />
+            </AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
+}
+
+export default PotaShell;
---
+++ src/components/HamInput.tsx
@@ -0,0 +1,82 @@
+import { Button, Checkbox, Divider, Group, TextInput, Title } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+const HamInput = ({
+    call,
+    setCall,
+    lat,
+    setLat,
+    long,
+    setLong,
+    onFindParksClicked,
+}: {
+    call: string;
+    setCall: React.Dispatch<React.SetStateAction<string>>;
+    lat: number;
+    setLat: React.Dispatch<React.SetStateAction<number>>;
+    long: number;
+    setLong: React.Dispatch<React.SetStateAction<number>>;
+    onFindParksClicked: () => void;
+}) => {
+    const form = useForm({
+        mode: "controlled",
+        initialValues: {
+            email: "",
+            termsOfService: false,
+        },
+
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+        },
+    });
+
+    return (
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            {/* <Title order={2}> Operator Info</Title> */}
+            <Divider my="xs" label="Operator Info" labelPosition="left" />
+
+            <TextInput
+                label="Callsign"
+                type="text"
+                placeholder="DN9CVR"
+                value={call}
+                onChange={(e) => setCall(e.target.value)}
+            />
+
+            <Divider my="xs" label="Location" labelPosition="left" />
+
+            <TextInput
+                label="Latitude"
+                type="number"
+                placeholder="50.819649"
+                value={lat}
+                onChange={(e) => setLat(parseFloat(e.target.value))}
+            />
+
+            <TextInput
+                label="Longitude"
+                type="number"
+                placeholder="6.97578"
+                value={long}
+                onChange={(e) => setLong(parseFloat(e.target.value))}
+            />
+
+            {/* <Divider my="xs" label="or" variant="dashed" />
+
+            <TextInput
+                label="Maidenhead Locator"
+                type="text"
+                placeholder="JO30lt"
+                value={call}
+                onChange={(e) => setCall(e.target.value)}
+            /> */}
+
+            <Group justify="flex-end" mt="md">
+                <Button type="button" onClick={onFindParksClicked}>
+                    Find Parks
+                </Button>
+            </Group>
+        </form>
+    );
+};
+export { HamInput };
