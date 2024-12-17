dadbe6a96904f351cc4fc6126837b41a4df42244
Christian Hoffmann <christian.hoffmann@sigmatechnology.com>
2024-12-17 12:58:39 +0100

    added grayscale, separated input state from app state

--- src/components/HamInput.tsx
+++ src/components/HamInput.tsx
@@ -2,50 +2,56 @@ import { Button, Divider, Group, Loader, TextInput } from "@mantine/core";
 import { useAppStore } from "../store/AppState";
 import { useShallow } from "zustand/shallow";
 import { useParksQuery } from "../clients/ParksClient";
+import { useState } from "react";
 
 const HamInput = () => {
-    const [call, setCall] = useAppStore(useShallow((state) => [state.call, state.setCall]));
-    const [lat, setLat] = useAppStore(useShallow((state) => [state.lat, state.setLat]));
-    const [long, setLong] = useAppStore(useShallow((state) => [state.long, state.setLong]));
-    const [radius, setRadius] = useAppStore(useShallow((state) => [state.radius, state.setRadius]));
+  const [appCall, setAppCall] = useAppStore(useShallow((state) => [state.call, state.setCall]));
+  const [appLat, setAppLat] = useAppStore(useShallow((state) => [state.lat, state.setLat]));
+  const [appLong, setAppLong] = useAppStore(useShallow((state) => [state.long, state.setLong]));
+  const [appRadius, setAppRadius] = useAppStore(useShallow((state) => [state.radius, state.setRadius]));
 
-    const setQth = useAppStore((state) => state.setQth);
+  const [call, setCall] = useState(appCall);
+  const [lat, setLat] = useState(appLat);
+  const [long, setLong] = useState(appLong);
+  const [radius, setRadius] = useState(appRadius);
 
-    const { isFetching: isParksFetching, refetch: fetchParks } = useParksQuery();
+  const setQth = useAppStore((state) => state.setQth); // TODO: remove
 
-    return (
-        <>
-            <Divider my="xs" label="Operator Info" labelPosition="left" />
+  const { isFetching: isParksFetching, refetch: fetchParks } = useParksQuery();
 
-            <TextInput
-                label="Callsign"
-                type="text"
-                placeholder="DN9CVR"
-                value={call}
-                onChange={(e) => {
-                    setCall(e.target.value);
-                }}
-            />
+  return (
+    <>
+      <Divider my="xs" label="Operator Info" labelPosition="left" />
 
-            <Divider my="xs" label="Location" labelPosition="left" />
+      <TextInput
+        label="Callsign"
+        type="text"
+        placeholder="DN9CVR"
+        value={call}
+        onChange={(e) => {
+          setCall(e.target.value);
+        }}
+      />
 
-            <TextInput
-                label="Latitude"
-                type="number"
-                placeholder="50.819649"
-                value={lat}
-                onChange={(e) => setLat(parseFloat(e.target.value))}
-            />
+      <Divider my="xs" label="Location" labelPosition="left" />
 
-            <TextInput
-                label="Longitude"
-                type="number"
-                placeholder="6.97578"
-                value={long}
-                onChange={(e) => setLong(parseFloat(e.target.value))}
-            />
+      <TextInput
+        label="Latitude"
+        type="number"
+        placeholder="50.819649"
+        value={lat}
+        onChange={(e) => setLat(parseFloat(e.target.value))}
+      />
 
-            {/* <Divider my="xs" label="or" variant="dashed" />
+      <TextInput
+        label="Longitude"
+        type="number"
+        placeholder="6.97578"
+        value={long}
+        onChange={(e) => setLong(parseFloat(e.target.value))}
+      />
+
+      {/* <Divider my="xs" label="or" variant="dashed" />
 
             <TextInput
                 label="Maidenhead Locator"
@@ -55,19 +61,24 @@ const HamInput = () => {
                 onChange={(e) => setCall(e.target.value)}
             /> */}
 
-            <Group justify="flex-end" mt="md">
-                <Button
-                    type="button"
-                    onClick={() => {
-                        setQth({ lat: lat, long: long });
-                        fetchParks();
-                    }}
-                    disabled={isNaN(lat) || isNaN(long) || call.length < 1 || isParksFetching}>
-                    {!isParksFetching && <>Find Parks</>}
-                    {isParksFetching && <Loader size={16} type="dots" />}
-                </Button>
-            </Group>
-        </>
-    );
+      <Group justify="flex-end" mt="md">
+        <Button
+          type="button"
+          onClick={() => {
+            setAppCall(call);
+            setAppLat(lat);
+            setAppLong(long);
+            setAppRadius(radius);
+            setQth({ lat: lat, long: long }); // TODO: remove
+            fetchParks();
+          }}
+          disabled={isNaN(lat) || isNaN(long) || call.length < 1 || isParksFetching}
+        >
+          {!isParksFetching && <>Find Parks</>}
+          {isParksFetching && <Loader size={16} type="dots" />}
+        </Button>
+      </Group>
+    </>
+  );
 };
 export { HamInput };
--- src/components/ParksView.tsx
+++ src/components/ParksView.tsx
@@ -3,20 +3,20 @@ import { ParksMap } from "./ParksMap";
 import { ParksTable } from "./ParksTable";
 
 const ParksView = () => {
-    return (
-        <Tabs defaultValue="map" style={{ flexGrow: "1", display: "flex", flexDirection: "column" }}>
-            <Tabs.List grow>
-                <Tabs.Tab value="map">Map</Tabs.Tab>
-                <Tabs.Tab value="table">Table</Tabs.Tab>
-            </Tabs.List>
-            <Tabs.Panel value="map" style={{ flexGrow: "1", display: "flex", flexDirection: "column" }}>
-                <ParksMap />
-            </Tabs.Panel>
-            <Tabs.Panel value="table" pt="xs">
-                <ParksTable />
-            </Tabs.Panel>
-        </Tabs>
-    );
+  return (
+    <Tabs defaultValue="map" style={{ flexGrow: "1", display: "flex", flexDirection: "column" }}>
+      <Tabs.List grow>
+        <Tabs.Tab value="map">🌍 Map</Tabs.Tab>
+        <Tabs.Tab value="table">📊 Table</Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Panel value="map" style={{ flexGrow: "1", display: "flex", flexDirection: "column" }}>
+        <ParksMap />
+      </Tabs.Panel>
+      <Tabs.Panel value="table" pt="xs">
+        <ParksTable />
+      </Tabs.Panel>
+    </Tabs>
+  );
 };
 
 export { ParksView };
